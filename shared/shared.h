1fd785e3464b42d4aa0ae85c84ef2ecd698cb918
Frouk3 <68140414+Frouk3@users.noreply.github.com>
2022-12-12 20:36:34 +0200

    Add files via upload

---
+++ shared/shared.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "GameFlags.h"
+#include "GameMenuStatus.h"
+#include "GameplayFlags.h"
+#include <Windows.h>
+
+namespace shared
+{
+	inline DWORD base = (DWORD)GetModuleHandleA(NULL);
+}
