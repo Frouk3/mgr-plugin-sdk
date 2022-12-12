af8830b56460a98e48cd05a8aa916ceb8217ad7e
Frouk3 <68140414+Frouk3@users.noreply.github.com>
2022-12-12 20:34:11 +0200

    Add files via upload

---
+++ game/cSlowRateManager.cpp
@@ -0,0 +1,47 @@
+#include "cSlowRateManager.h"
+#include "shared.h"
+#include <Windows.h>
+
+void cSlowRateManager_SetSlowRate(int SlowRateType, float SlowRate) noexcept
+{
+	DWORD address = shared::base + 0x532020;
+	((void(__cdecl*)(int, float))address)(SlowRateType, SlowRate);
+}
+
+cSlowRateManager* GetcSlowRateManager() noexcept
+{
+	DWORD address = shared::base + 0xA03960;
+	return ((cSlowRateManager *(__stdcall*)())address)();
+}
+
+void cSlowRateManager::SetSlowRate(int SlowRateType, float SlowRate) noexcept
+{
+	DWORD address = shared::base + 0xA03A70;
+	((void(__thiscall*)(cSlowRateManager*, int, float))address)(this, SlowRateType, SlowRate);
+}
+
+float cSlowRateManager::GetSlowRate(int SlowRateType) noexcept
+{
+	if (SlowRateType < 4)
+		return this->m_fSlowRate[SlowRateType].m_fRate;
+	else
+		return 1.0f;
+}
+
+void cSlowRateManager::ResetSlowRate() noexcept
+{
+	DWORD address = shared::base + 0xA03B10;
+	((void(__thiscall*)(cSlowRateManager*))address)(this);
+}
+
+void cSlowRateManager::Cleanup() noexcept
+{
+	DWORD address = shared::base + 0xA08740;
+	((void(__thiscall*)(cSlowRateManager*))address)(this);
+}
+
+void cSlowRateManager_Reset() noexcept
+{
+	DWORD address = shared::base + 0x11EDC20;
+	((void(__cdecl*)())address)();
+}
---
+++ game/cSlowRateManager.h
@@ -0,0 +1,42 @@
+#pragma once
+
+class cSlowRateManager
+{
+public:
+  void *vtbl;
+  int field_4;
+  int field_8;
+  int field_C;
+  int field_10;
+  int field_14;
+  int field_18;
+  int field_1C;
+  int field_20;
+  int field_24;
+  int field_28;
+  int field_2C;
+  int field_30;
+  int field_34;
+  int field_38;
+  struct {
+		float m_fRate;
+		float m_fDefaultRate;
+		float m_fActualRate;
+		float m_fCalculatedRate;
+	} m_fSlowRate[4];
+  float m_fTickRate;
+  float m_fTicks;
+  float field_84;
+  float field_88;
+  float field_8C;
+  float field_90;
+
+  void SetSlowRate(int SlowRateType, float SlowRate) noexcept;
+  float GetSlowRate(int SlowRateType) noexcept;
+  void ResetSlowRate() noexcept;
+  void Cleanup() noexcept;
+};
+
+void cSlowRateManager_Reset() noexcept;
+void cSlowRateManager_SetSlowRate(int SlowRateType, float SlowRate) noexcept;
+cSlowRateManager* GetcSlowRateManager() noexcept;
