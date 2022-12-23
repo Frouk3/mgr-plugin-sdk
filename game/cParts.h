c9902b46a80ca471665392e1d2790541389c690f
Frouk3 <68140414+Frouk3@users.noreply.github.com>
2022-12-23 15:17:24 +0200

    Create cParts.h

---
+++ game/cParts.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "cVec4.h"
+
+class cParts
+{
+public:
+  void *__vftable /*VFT*/;
+  int field_4;
+  int field_8;
+  int field_C;
+  float field_10;
+  float field_14;
+  float field_18;
+  float field_1C;
+  float field_20;
+  float field_24;
+  float field_28;
+  float field_2C;
+  float field_30;
+  float field_34;
+  float field_38;
+  float field_3C;
+  cVec4 m_vecPos;
+  cVec4 m_vecPosition;
+  float field_60;
+  float field_64;
+  float field_68;
+  float field_6C;
+  cVec4 m_vecSize;
+  float field_80;
+  float field_84;
+  float field_88;
+  int field_8C;
+  float field_90;
+  float field_94;
+  float field_98;
+  float field_9C;
+  __int16 field_A0;
+  __int16 field_A2;
+  int field_A4;
+  int field_A8;
+  int field_AC;
+};
