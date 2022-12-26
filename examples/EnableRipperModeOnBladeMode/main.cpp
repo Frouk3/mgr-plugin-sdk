b12a26398e8f746b8c311b1a692d155178f3e546
Frouk3 <luminx02@gmail.com>
2022-12-26 21:23:33 +0200

    upd

--- examples/EnableRipperModeOnBladeMode/main.cpp
+++ examples/EnableRipperModeOnBladeMode/main.cpp
@@ -1,9 +1,13 @@
 #include "Pl0000.h"
+#include "cGameUIManager.h"
 #include "shared/shared.h"
 
+// i didn't check if it its works, just robbed a quick one :O
 void RipperModeOnBladeMode()
 {
-    Pl0000 *player = *(Pl0000**)(shared::base + 0x19C1490);
+    cGameUIManager& GameUIManager = *(cGameUIManager*)(shared::base + 0x19C1430);
+
+    Pl0000 *player = (Pl0000*)GameUIManager.m_pPl;
 
     if (player)
     {
---
+++ game/cGameUIManager.h
@@ -0,0 +1,62 @@
+#pragma once
+
+class cGameUIManager
+{
+protected:
+  void *vtbl;
+public:
+  int field_4;
+  int field_8;
+  int field_C;
+  int field_10;
+  int field_14;
+  int field_18;
+  int field_1C;
+  int field_20;
+  int field_24;
+  int field_28;
+  int field_2C;
+  int field_30;
+  int field_34;
+  int field_38;
+  int field_3C;
+  int field_40;
+  int field_44;
+  int field_48;
+  int field_4C;
+  int field_50;
+  int field_54;
+  int field_58;
+  int field_5C;
+  void* m_pPl;
+  int field_64;
+  int field_68;
+  int field_6C;
+  int field_70;
+  int field_74;
+  int field_78;
+  int field_7C;
+  int field_80;
+  int field_84;
+  int field_88;
+  int field_8C;
+  int field_90;
+  int field_94;
+  void *m_pPlayer;
+  int field_9C;
+  float field_A0;
+  float field_A4;
+  float field_A8;
+  float field_AC;
+  float field_B0;
+  float field_B4;
+  float field_B8;
+  float field_BC;
+  int field_C0;
+  int field_C4;
+  int field_C8;
+  int field_CC;
+  int field_D0;
+  int field_D4;
+  int field_D8;
+};
