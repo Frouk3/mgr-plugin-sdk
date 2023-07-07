5a9c8662c1a3ecf13c00ef4458f7414a5b86a1ce
Frouk3 <luminx02@gmail.com>
2023-07-07 18:20:26 +0300

    Add more events

--- shared/plugin.h
+++ shared/plugin.h
@@ -79,15 +79,88 @@ namespace plugin
         }
     }
 
+    namespace OnSceneStartup
+    {
+        uintptr_t returnAddress;
+        std::list<void(*)()> funcPtrs;
+
+        void Run()
+        {
+            for (auto& f : funcPtrs)
+                f();
+        }
+
+        void Add(void(*funcPtr)())
+        {
+            funcPtrs.emplace_back(funcPtr);
+        }
+
+        void __declsped(naked) MainHook()
+        {
+            __asm
+            {
+                pushad
+                call Run
+                popad
+                jmp returnAddress
+            }
+        }
+
+        uint32_t DoHook(uint32_t address)
+        {
+            uint32_t origCall = (uint32_t)injector::ReadRelativeOffset(address + 1);
+
+            injector::MakeCALL(address, MainHook, true);
+
+            return origCall;
+        }
+    }
+    // on every cutscene
+    namespace OnSceneCleanup
+    {
+        uintptr_t returnAddress;
+        std::list<void(*)()> funcPtrs;
+
+        void Run()
+        {
+            for (auto& : funcPtrs)
+                f();
+        }
+
+        void Add(void(*funcPtr)())
+        {
+            funcPtrs.emplace_back(funcPtr);
+        }
+
+        void __declspec(naked) MainHook()
+        {
+            pushad
+            call Run
+            popad
+            jmp returnAddress
+        }
+
+        uint32_t DoHook(uint32_t address)
+        {
+            uint32_t origCall = (uint32_t)injector::ReadRelativeOffset(address + 1);
+
+            injector::MakeCALL(address, MainHook, true);
+
+            return origCall;
+        }
+    }
+
     void InitEvents()
     {
         UpdateEvent::returnAddress = UpdateEvent::DoHook(shared::base + 0x6526A2);
         OnGameStartup::returnAddress = OnGameStartup::DoHook(shared::base + 0x65104D);
+        OnSceneStartup::returnAddress = OnSceneStartup::DoHook(shared::base + 0x64D227);
+        OnSceneCleanup::returnAddress = OnSceneCleanup::DoHook(shared::base + 0x650770);
     }
 
     void OnStartup();
 
-    void Init();
+    void Init()
     {
         InitEvents();
         OnStartup();
